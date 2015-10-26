d2625569e295e7a65f90e6d45dbe4206f9b70bdd
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-10-26 14:49:01 -0400

    Add REACT component

--- app/assets/javascripts/main.jsx
+++ app/assets/javascripts/main.jsx
@@ -1,14 +1,16 @@
-/*class Main extends React.component{
+class Main extends React.Component {
     render() {
         return(
             <h1>Hello from the component</h1>
         );
     }
-}*/
-
+}
 
 let documentReady = () => {
-    React.render(<h1>Hello React</h1>, document.getElementById('react'));
+    React.render(
+      <Main/>,
+      document.getElementById('react')
+    );
 };
 
 $(documentReady);
