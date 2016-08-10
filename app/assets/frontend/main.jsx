da677250885fee3dcce294bc2d18c38e236aa562
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-08-10 18:24:04 -0400

    Adding Time and Gravatar

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -6,18 +6,29 @@ class Main extends React.Component {
 		super(props);
 		this.state = { tweetsList: [] };
 	}
+	formattedTweets(tweetsList){
+		let formattedList = tweetsList.map(tweet => {
+			tweet.formattedDate = moment(tweet.created_at).fromNow();
+			return tweet;
+		});
+		return{
+			tweetsList: formattedList
+		};
+	}
 	addTweet(tweetToAdd){
 		$.post("/tweets", { body: tweetToAdd })
 		.success( savedTweet => {
 			let newTweetsList = this.state.tweetsList;
 			newTweetsList.unshift(savedTweet);
-			this.setState({ tweetsList: newTweetsList });
+			//this.setState({ tweetsList: newTweetsList });
+			this.setState(this.formattedTweets(newTweetsList));
 		})
 		.error(error => console.log(error));
 	}
 	componentDidMount() {
 		$.ajax("/tweets")
-		.success(data => this.setState({ tweetsList: data }))
+		//.success(data => this.setState({ tweetsList: data }))
+		.success(data => this.setState( this.formattedTweets(data) ))
 		.error(error => console.log(error));
 	}
     render() {
--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -80,6 +80,17 @@
 		}
 	
 		_createClass(Main, [{
+			key: "formattedTweets",
+			value: function formattedTweets(tweetsList) {
+				var formattedList = tweetsList.map(function (tweet) {
+					tweet.formattedDate = moment(tweet.created_at).fromNow();
+					return tweet;
+				});
+				return {
+					tweetsList: formattedList
+				};
+			}
+		}, {
 			key: "addTweet",
 			value: function addTweet(tweetToAdd) {
 				var _this2 = this;
@@ -87,7 +98,8 @@
 				$.post("/tweets", { body: tweetToAdd }).success(function (savedTweet) {
 					var newTweetsList = _this2.state.tweetsList;
 					newTweetsList.unshift(savedTweet);
-					_this2.setState({ tweetsList: newTweetsList });
+					//this.setState({ tweetsList: newTweetsList });
+					_this2.setState(_this2.formattedTweets(newTweetsList));
 				}).error(function (error) {
 					return console.log(error);
 				});
@@ -97,8 +109,10 @@
 			value: function componentDidMount() {
 				var _this3 = this;
 	
-				$.ajax("/tweets").success(function (data) {
-					return _this3.setState({ tweetsList: data });
+				$.ajax("/tweets")
+				//.success(data => this.setState({ tweetsList: data }))
+				.success(function (data) {
+					return _this3.setState(_this3.formattedTweets(data));
 				}).error(function (error) {
 					return console.log(error);
 				});
@@ -292,15 +306,16 @@
 				return React.createElement(
 					"li",
 					{ className: "collection-item avatar" },
-					React.createElement(
-						"i",
-						{ className: "material-icons circle" },
-						"person_pin "
-					),
+					React.createElement("img", { className: "circle", src: this.props.gravatar }),
 					React.createElement(
 						"span",
 						{ className: "title" },
 						this.props.name
+					),
+					React.createElement(
+						"time",
+						null,
+						this.props.formattedDate
 					),
 					React.createElement(
 						"p",
